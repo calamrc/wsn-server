c79a3c4b999e2dd1c67bc94709ca00aa0e6ec74f
Ramon Cristopher Calam <ramoncristopher.calam@g.msuiit.edu.ph>
2020-09-12 11:52:05 +0800

    Reset database.

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+class Sensor(models.Model):
+    sensor_id = models.CharField(max_length=10)
+    type = models.CharField(max_length=20)
+    unit = models.CharField(max_length=20)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+
+class WSN(models.Model):
+    wsn_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    sensors = models.ManyToManyField(Sensor, through="WSNDetails")
+
+    def __str__(self):
+        return self.name
+
+class WSNDetails(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    wsn = models.ForeignKey(WSN, on_delete=models.CASCADE)
+
--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+# from .models import WSNDetails
+
+import time
+import json
 
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
+        # dataJSON = request.body
+        # dataDict = json.loads(dataJSON)
+        # data = WSNDetails.objects.create(**dataDict)
         return HttpResponse('Hello {}'.format(request.body))
 
     return render(request, "index.html")
 
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
