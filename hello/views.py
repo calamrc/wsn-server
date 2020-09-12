04531d3ff6a2a11d023ef7cfd7756f23431ac5c6
Ramon Cristopher Calam <ramoncristopher.calam@g.msuiit.edu.ph>
2020-09-12 20:57:40 +0800

    Fix responses.

--- hello/views.py
+++ hello/views.py
@@ -9,51 +9,57 @@ import json
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        dataJSON = request.body
-        dataDict = json.loads(dataJSON)
+        try:
+            dataJSON = request.body
+            dataDict = json.loads(dataJSON)
 
-        wsnDetails = {
-            "wsn_id": dataDict.get("id"),
-            "name": dataDict.get("name"),
-            "location": dataDict.get("location")
-        }
-
-        newWSN = WSN.objects.create(**wsnDetails)
-
-        sensors = dataDict.get("sensors")
-        for sensor in sensors:
-            sensorDetails = {
-                "sensor_id": sensor.get("id"),
-                "type": sensor.get("type"),
-                "unit": sensor.get("unit"),
-                "value": sensor.get("value"),
+            wsnDetails = {
+                "wsn_id": dataDict.get("id"),
+                "name": dataDict.get("name"),
+                "location": dataDict.get("location")
             }
-            newSensor = Sensor.objects.create(**sensorDetails)
-            w = WSNDetails(sensor=newSensor, wsn=newWSN)
-            w.save()
 
-        return HttpResponse('Hello {}'.format(w.when))
+            newWSN = WSN.objects.create(**wsnDetails)
+
+            sensors = dataDict.get("sensors")
+            for sensor in sensors:
+                sensorDetails = {
+                    "sensor_id": sensor.get("id"),
+                    "type": sensor.get("type"),
+                    "unit": sensor.get("unit"),
+                    "value": sensor.get("value"),
+                }
+                newSensor = Sensor.objects.create(**sensorDetails)
+                w = WSNDetails(sensor=newSensor, wsn=newWSN)
+                w.save()
+
+            return HttpResponse("Added")
+        except Exception as e:
+            return HttpResponse("Error: {}".format(e))
     else:
-        return HttpResponse("")
+        return HttpResponse("Not Supported!")
 
 def fetch(request, wsn_id):
     if request.method == "GET":
-        wsnDetails = WSNDetails.objects.order_by("-when").filter(wsn__wsn_id=wsn_id)[0]
-        sensors = []
-        for sensor in wsnDetails.wsn.sensors.all():
-            sensors.append({
-                "id": sensor.sensor_id,
-                "type": sensor.type,
-                "unit": sensor.unit,
-                "value": float(sensor.value)
-            })
-        dataDict = {
-            "id": wsnDetails.wsn.wsn_id,
-            "name": wsnDetails.wsn.name,
-            "location": wsnDetails.wsn.location,
-            "sensors": sensors
-        }
-        return HttpResponse(json.dumps(dataDict))
+        try:
+            wsnDetails = WSNDetails.objects.order_by("-when").filter(wsn__wsn_id=wsn_id)[0]
+            sensors = []
+            for sensor in wsnDetails.wsn.sensors.all():
+                sensors.append({
+                    "id": sensor.sensor_id,
+                    "type": sensor.type,
+                    "unit": sensor.unit,
+                    "value": float(sensor.value)
+                })
+            dataDict = {
+                "id": wsnDetails.wsn.wsn_id,
+                "name": wsnDetails.wsn.name,
+                "location": wsnDetails.wsn.location,
+                "sensors": sensors
+            }
+            return HttpResponse(json.dumps(dataDict))
+        except Exception as e:
+            return HttpResponse("Error: {}".format(e))
     else:
-        return HttpResponse("")
+        return HttpResponse("Not Supported!")
 
