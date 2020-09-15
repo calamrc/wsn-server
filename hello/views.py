5e3eafa2440f2e82aa3be58c95179f3ef0fda5c9
Ramon Cristopher Calam <ramoncristopher.calam@g.msuiit.edu.ph>
2020-09-15 14:39:23 +0800

    Debug.

--- hello/views.py
+++ hello/views.py
@@ -38,7 +38,7 @@ def index(request):
             return HttpResponse("Error: {}".format(e))
     elif request.method  == "GET":
         try:
-            wsnDetails = WSNDetails.objects.all()
+            wsnDetails = WSNDetails.objects.order_by("-when")
             # nodes = {
                 # "nodes": [],
             # }
@@ -48,7 +48,7 @@ def index(request):
                     # nodes["nodes"].append(wsn_id)
 
             # return HttpResponse(json.dumps(nodes))
-            return HttpResponse(wsnDetails.wsn)
+            return HttpResponse(wsnDetails)
         except Exception as e:
             return HttpResponse("Error: {}".format(e))
     else:
